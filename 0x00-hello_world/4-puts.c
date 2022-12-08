6104ff80fe9c9d1f2f9e705d6a31124192c2e3f1
Lsssdi <lesedimokoana@gmail.com>
2022-12-08 06:42:51 -0800

    commiting 4-puts.c to repo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * prints string to std output
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
