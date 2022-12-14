da77bdc34564f45d124d89471d411f34cf4b635e
Lsssdi <lesedimokoana@gmail.com>
2022-12-14 07:29:12 -0800

    commiting corrected 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,16 @@
-#include "main.h" 
+#include "main.h"
 
 /**
- * main - Entry point 
+ * main - Entry point
  *
- *prrints outs '_putchar' followed by a new line 
+ *prrints outs '_putchar' followed by a new line
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	char word[]= {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\n'};
+	char word[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\n'};
 	int i = 0;
 
 	for (; i < 9; i++)
