730bbb53950abbd04e21c47739f222d38da54470
Lsssdi <lesedimokoana@gmail.com>
2022-12-14 07:41:25 -0800

    commiting changed 6-abs.c to repo

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 
 /**
  * _abs - returns the absolute value of an integer
- *
+ *@n: an integer
  * Return: absolute value of entered value.
  */
 
@@ -16,7 +16,7 @@ int _abs(int n)
 	}
 	else if (n < 0)
 	{
-		number = -1*n;
+		number = -1 * n;
 	}
 
 	return (number);
