c736de237c964d331b8d0d2863cd0de7a40ee636
Lsssdi <lesedimokoana@gmail.com>
2022-12-20 06:58:38 -0800

    commiting 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * *print_array - prinst the first n elemnts of an array of integers
+ * @a: address of array
+ * @n: number of elementsv to be displayed
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+		printf("%d", *a);
+		a++;
+
+		if ( i < n - 1)
+			printf(", ");
+	}
+
+	putchar('\n');
+}
