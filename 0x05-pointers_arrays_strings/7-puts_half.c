74a19a3ca5dc890193aafa1359d36823573faa28
Lsssdi <lesedimokoana@gmail.com>
2022-12-20 06:34:41 -0800

    commiting 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: pointer to a string
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int l = strlen(str);
+
+	if (l % 2 == 0)
+		i = (l / 2);
+	else
+		i = (l - 1) / 2;
+
+	while (i < l)
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+
+	_putchar('\n');
+}
