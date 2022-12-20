11c5f091b64b8223efbae7b0c998cbcec7180cf5
Lsssdi <lesedimokoana@gmail.com>
2022-12-20 00:44:09 -0800

    commiting 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: a pointer to a string
+ *
+ * Return: the length of a string as int
+ */
+
+int _strlen(char *s)
+{
+	return(strlen(s));
+}
