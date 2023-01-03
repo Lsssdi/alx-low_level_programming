5208dbe90a5c4e37f3f0dedb81a704f163a0355e
Lsssdi <lesedimokoana@gmail.com>
2023-01-03 06:26:12 -0800

    commiting 6-strstr.c to repo

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,38 @@
+/**
+ * _strstr - finds substring within another string
+ * @haystack: pointer to string to be searched
+ * @needle: pointer to substring to find
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int l = 0;
+	char *ptr = 0;
+
+	while (*(needle + l) != '\0')
+		l++;
+
+	while (*haystack != '\0')
+	{
+		if (*haystack == *needle)
+		{
+			int i = 0;
+
+			while (*(haystack + i) == *(needle + i) &&
+					*(needle + i) != '\0' &&
+					*(haystack + i) != '\0')
+				i++;
+
+			if (i == l)
+			{
+				ptr = haystack;
+				break;
+			}
+		}
+
+		haystack++;
+	}
+
+	return (ptr);
+
+}
