bdbc59884c3fc5cf47cb566e6e2c19c827f50ace
Lsssdi <lesedimokoana@gmail.com>
2023-01-03 00:58:20 -0800

    commiting 2-strchr to repo

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+/**
+ * _strchr(char *s, char c)
+ * @s: pointer to a string
+ * @c: character to search for in string
+ *
+ * Return: pointer to address of first occurence of 
+ * charcter in string else return NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	char *strAddr = 0;
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			strAddr = s;
+			break;
+		}
+
+		s++;
+	}
+
+	return (strAddr);
+}
