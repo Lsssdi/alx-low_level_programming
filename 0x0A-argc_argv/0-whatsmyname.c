c2564cceb06db8a77e5f04a656308a56abc1007f
Lsssdi <lesedimokoana@gmail.com>
2023-01-09 09:32:42 -0800

    m

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point to programme
+ *
+ * Return: 0 (Always Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", *argv);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+The files in this repository are related to the "0x0A. C - argc, argv" project
+of the ALX software engineering programme.
